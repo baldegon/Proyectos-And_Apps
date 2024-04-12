820912d75731e03d36986ca5ec04fdd3bc6206ec
Bladimir Holmann Zink <114711795+baldegon@users.noreply.github.com>
2024-04-12 19:03:35 -0300

    Adding new simple project to the portfolio
    To Do List App. new updates in the future.

---
+++ To Do List/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace To_Do_List
+{
+    partial class ToDoList
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Title = new System.Windows.Forms.Label();
+            this.Nametxt = new System.Windows.Forms.TextBox();
+            this.NameTask = new System.Windows.Forms.Label();
+            this.DescriptionTask = new System.Windows.Forms.Label();
+            this.Descriptiontxt = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.toDoListView = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.toDoListView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Title);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(630, 123);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.BackColor = System.Drawing.Color.Transparent;
+            this.Title.Font = new System.Drawing.Font("Myanmar Text", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(200, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(203, 66);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "To Do List";
+            // 
+            // Nametxt
+            // 
+            this.Nametxt.Location = new System.Drawing.Point(17, 173);
+            this.Nametxt.Name = "Nametxt";
+            this.Nametxt.Size = new System.Drawing.Size(252, 20);
+            this.Nametxt.TabIndex = 1;
+            // 
+            // NameTask
+            // 
+            this.NameTask.AutoSize = true;
+            this.NameTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameTask.Location = new System.Drawing.Point(13, 146);
+            this.NameTask.Name = "NameTask";
+            this.NameTask.Size = new System.Drawing.Size(71, 24);
+            this.NameTask.TabIndex = 2;
+            this.NameTask.Text = "Name:";
+            this.NameTask.Click += new System.EventHandler(this.NameTask_Click);
+            // 
+            // DescriptionTask
+            // 
+            this.DescriptionTask.AutoSize = true;
+            this.DescriptionTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DescriptionTask.Location = new System.Drawing.Point(13, 205);
+            this.DescriptionTask.Name = "DescriptionTask";
+            this.DescriptionTask.Size = new System.Drawing.Size(121, 24);
+            this.DescriptionTask.TabIndex = 4;
+            this.DescriptionTask.Text = "Description:";
+            // 
+            // Descriptiontxt
+            // 
+            this.Descriptiontxt.Location = new System.Drawing.Point(17, 232);
+            this.Descriptiontxt.Name = "Descriptiontxt";
+            this.Descriptiontxt.Size = new System.Drawing.Size(252, 20);
+            this.Descriptiontxt.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(25, 295);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(58, 22);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(108, 295);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(58, 22);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(188, 295);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(58, 22);
+            this.btnEdit.TabIndex = 8;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(264, 295);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(58, 22);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // toDoListView
+            // 
+            this.toDoListView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.toDoListView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.toDoListView.Location = new System.Drawing.Point(17, 324);
+            this.toDoListView.Name = "toDoListView";
+            this.toDoListView.Size = new System.Drawing.Size(625, 239);
+            this.toDoListView.TabIndex = 10;
+            // 
+            // ToDoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(654, 584);
+            this.Controls.Add(this.toDoListView);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.DescriptionTask);
+            this.Controls.Add(this.Descriptiontxt);
+            this.Controls.Add(this.NameTask);
+            this.Controls.Add(this.Nametxt);
+            this.Controls.Add(this.panel1);
+            this.Name = "ToDoList";
+            this.Text = "To Do List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.toDoListView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.TextBox Nametxt;
+        private System.Windows.Forms.Label NameTask;
+        private System.Windows.Forms.Label DescriptionTask;
+        private System.Windows.Forms.TextBox Descriptiontxt;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView toDoListView;
+    }
+}
+
---
+++ To Do List/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace To_Do_List
+{
+    public partial class ToDoList : Form
+    {
+        public ToDoList()
+        {
+            InitializeComponent();
+        }
+
+        DataTable todolist = new DataTable();
+        bool isEditing = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Create Columns
+            todolist.Columns.Add("NameTask");
+            todolist.Columns.Add("DescriptionTask");
+
+            //Point our datagridview to our datasource
+            toDoListView.DataSource = todolist;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void NameTask_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Nametxt.Text = "";
+            Descriptiontxt.Text = "";
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            isEditing = true;
+            // Fill text fields with data from table
+            Nametxt.Text = todolist.Rows[toDoListView.CurrentCell.RowIndex].ItemArray[0].ToString();
+            Descriptiontxt.Text = todolist.Rows[toDoListView.CurrentCell.RowIndex].ItemArray[1].ToString();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                todolist.Rows[toDoListView.CurrentCell.RowIndex].Delete();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (isEditing)
+            {
+                todolist.Rows[toDoListView.CurrentCell.RowIndex]["NameTask"] = Nametxt.Text;
+                todolist.Rows[toDoListView.CurrentCell.RowIndex]["DescriptionTask"] = Descriptiontxt.Text;
+            }
+            else
+            {
+                todolist.Rows.Add(Nametxt.Text, Descriptiontxt.Text);
+            }
+            // Clear Fields
+            Nametxt.Text = "";
+            Descriptiontxt.Text = "";
+            isEditing = false;  
+        }
+    }
+}
